42f18ca0f81350e3f4fd725c7b42fa48e1339219
vishu2003 <justvishu2003@gmail.com>
2024-11-24 21:31:12 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const express = require ("express");
+const app = express ();
+const port = process.env.PORT || 3000;
+app.get ("/", async (req, res) => {
+    res.send("Hello from the Vishwajeet");
+})
+app.listen(port, ()=>{
+    console.log(`Connection is Live at port ${port}`);
+})
