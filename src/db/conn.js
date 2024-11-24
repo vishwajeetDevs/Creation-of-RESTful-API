198a036cb49f18a322f4e2e24b654db61d8e5607
vishu2003 <justvishu2003@gmail.com>
2024-11-24 22:17:03 +0530

    connection is successful

---
+++ src/db/conn.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/olympics").then(() => {
+    console.log("Connection Successful...");
+}).catch((e) => {
+    console.log("No Connection...");
+})
